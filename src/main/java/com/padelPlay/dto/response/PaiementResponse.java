1c9834451e311d338c741f6feb8cc2b77f5477b1
Ruser <user@DESKTOP-NS5BTI0>
2026-04-14 09:14:25 +0200

    bien

---
+++ src/main/java/com/padelPlay/dto/response/PaiementResponse.java
@@ -0,0 +1,18 @@
+package com.padel.padel_backend.dto.response;
+
+import com.padel.padel_backend.entity.enums.StatutPaiement;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaiementResponse {
+    private Long id;
+    private Double montant;
+    private StatutPaiement statut;
+    private LocalDateTime datePaiement;
+}
