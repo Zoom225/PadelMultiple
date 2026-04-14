cd69972f39fc1c4ed7c1563d405ab4d8a646ceca
Ruser <user@DESKTOP-NS5BTI0>
2026-04-14 11:28:57 +0200

    bien

---
+++ src/main/java/com/padelPlay/entity/enums/TypeAdministrateur.java
@@ -0,0 +1,6 @@
+package com.padel.padel_backend.entity.enums;
+
+public enum TypeAdministrateur {
+    GLOBAL,  // voit et gère tous les sites
+    SITE     // gère son site uniquement
+}
---
+++ src/main/java/com/padelPlay/entity/enums/TypeMatch.java
@@ -0,0 +1,6 @@
+package com.padel.padel_backend.entity.enums;
+
+public enum TypeMatch {
+    PRIVE,
+    PUBLIC
+}
