ab58a13275569b392700e651ab50d75231ade61d
mise-a-jour-backend <bobypro225@gmail.com>
2026-05-23 18:17:20 +0200

    tout fonctionne bien maintenent

--- src/main/java/com/padelPlay/match/dto/MatchDto.java
+++ src/main/java/com/padelPlay/match/dto/MatchDto.java
@@ -2,18 +2,23 @@ package com.padelPlay.match.dto;
 
 import com.padelPlay.entity.enums.StatutMatch;
 import com.padelPlay.entity.enums.TypeMatch;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public record MatchDto(
-    Long id,
-    Long terrainId,
-    String nomTerrain,
-    Long organisateurId,
-    String nomOrganisateur,
-    LocalDateTime dateDebut,
-    LocalDateTime dateFin,
-    TypeMatch typeMatch,
-    StatutMatch statut,
-    Integer nbJoueursActuels,
-    Double prixParJoueur
+        Long id,
+        Long terrainId,
+        String terrainNom,
+        String siteNom,
+        Long organisateurId,
+        String organisateurNom,
+        LocalDate date,
+        LocalTime heureDebut,
+        LocalTime heureFin,
+        TypeMatch typeMatch,
+        StatutMatch statut,
+        Integer nbJoueursActuels,
+        Double prixParJoueur,
+        LocalDateTime dateConversionPublic
 ) {}
